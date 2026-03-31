b51bcbcdd53bf7a485fc7e1b379a39a6f38eb93f
N1KITA2104 <herobrineset21@gmail.com>
2026-04-01 00:59:18 +0200

    Initialized "Task Manager" CRUD base

---
+++ src/main/java/com/example/springbootcrudbasetaskmanager/SpringBootCrudBaseTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcrudbasetaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudBaseTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudBaseTaskManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootcrudbasetaskmanager/SpringBootCrudBaseTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcrudbasetaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudBaseTaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
